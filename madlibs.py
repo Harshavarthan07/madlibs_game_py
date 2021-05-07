49bc21c7bd559c63579b1369068082f51a98b968
Harsha varthan <70424421+Harshavarthan07@users.noreply.github.com>
2021-05-07 14:56:50 +0530

    Add files via upload
    Added .py file

---
+++ madlibs.py
@@ -0,0 +1,21 @@
+#HALLOWEEN MAD LIBS
+day = input("What day it is: ")
+noun1 = input("noun: ")
+place = input("place: ")
+person = input("person: ")
+adj1 = input("Adjective: ")
+bodypart = input("bodypart: ")
+verb = input("verb: ")
+adj2 = input("Adjective: ")
+noun2 = input("noun: ")
+food = input("food: ")
+plural_noun = input("plural noun: ")
+
+
+madlibs = f"I can't believe it's already {day}! I can't wait to put on my {noun1} and visit every {place} in my neighborhood," \
+          f"This year, I am going to dress up as {person} with {adj1} {bodypart}. Before I {verb} ," \
+          f"I make sure to grab my {adj2} {noun2} to hold all of my {food}." \
+          f"Finally, all of my {plural_noun} are ready to go!"
+
+
+print(madlibs)
